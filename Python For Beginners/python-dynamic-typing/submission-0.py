180f8da1dc4cb0eb851f0275cd8fe46cf405835a
Umar <omian3@gmail.com>
2026-08-21 08:16:26 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.14
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Calc3"
+print(type(variable))
+variable = [6, 7]
+print(type(variable))
